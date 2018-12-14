ffeea3a97dd3fe0d533a02de4ad785a21cd16dfd
zachlloyd01 <zachlloyd01@gmail.com>
2018-12-14 04:37:40 -0500

    Update Img Upload code

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=140)
-    preview = models.CharField(max_length=300, default="Preview here!")
+    preview = models.CharField(max_length=300, default="")
     body = models.TextField()
     date = models.DateTimeField()
-    image = models.ImageField(default='')
+    image = models.ImageField(upload_to = "myblog/img")
 
     def __str__(self):
         return self.title
